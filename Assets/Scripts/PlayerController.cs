83b48b4f089060a286ffb325094d057630380c8a
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-10-06 20:32:44 -0400

    added respawn and pickup float

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {   
@@ -60,12 +61,16 @@ public class PlayerController : MonoBehaviour
 
         // Stop movement if speed is too low
         if (Input.GetAxisRaw("Horizontal") == 0f && Input.GetAxisRaw("Vertical") == 0f) {
-            rb.velocity = new Vector3(0.1f, rb.velocity.y, 0.1f);
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+        }
+
+        if (transform.position.y <= 0.5f){
+             SceneManager.LoadScene( SceneManager.GetActiveScene().name );
         }
 
     }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
-{
+{   
+    public float degreesPerSecond = 15.0f;
+    public float frequency = 1f;
+    public float height = 5f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3 ();
+    Vector3 tempPos = new Vector3 ();
+ 
+    // Use this for initialization
+    void Start () {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+         // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+ 
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * height;
+ 
+        transform.position = tempPos;
     }
 }
